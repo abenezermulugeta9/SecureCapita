19730314b5960fa6718037bedcf41b0e097f42f6
AbenezerM <abenezermulugeta41@gmail.com>
2023-06-30 10:35:43 -0400

    Add custom authorization filter to set the authenticated user in the context

---
+++ src/main/java/com/abenezermulugeta/securecapita/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * @author Abenezer Sefinew
+ * @version 1.0
+ * @since 06/09/2023
+ */
+
+package com.abenezermulugeta.securecapita.filter;
+
+import com.abenezermulugeta.securecapita.provider.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+    private static final String TOKEN_KEY = "token";
+    private static final String EMAIL_KEY = "email";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            Map<String, String> values = getRequestValues(request);
+            String token = getTokenFromRequest(request);
+            if(tokenProvider.isTokenValid(values.get(EMAIL_KEY), token)) {
+                List<GrantedAuthority> authorities = tokenProvider.getAuthorities(values.get(TOKEN_KEY));
+
+                // Setting the Authentication object for the servlet/ request, this can be accessed from anywhere in the code.
+                Authentication authentication = tokenProvider.getAuthentication(values.get(EMAIL_KEY), authorities, request);
+
+                // Set the authenticated user to the context
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            // Allowing the filter chain to continue to the next filters in the pipeline
+            filterChain.doFilter(request, response);
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+//            processError(request, response, exception);
+        }
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        return null;
+    }
+
+    private Map<String, String> getRequestValues(HttpServletRequest request) {
+
+    }
+}
